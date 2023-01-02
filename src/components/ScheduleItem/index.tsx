d9f586101532628325c94d7bba279fca2d90a2b9
woojung007 <eeeeve007@gmail.com>
2023-01-02 13:38:18 +0900

    메인페이지  라우터 추가

--- src/components/ScheduleItem/index.tsx
+++ src/components/ScheduleItem/index.tsx
@@ -5,40 +5,43 @@ import {
 } from "@ant-design/icons";
 import { Tag } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/pages/main.scss";
 
 const ScheduleItem = () => {
   return (
-    <div className="main_item_wrapper">
-      <div className="main_item_info">
-        <strong className="main_item_info_time">15시 00분</strong>
-        <strong>치아 검진</strong>
+    <Link to={"/reservation/id"}>
+      <div className="main_item_wrapper">
+        <div className="main_item_info">
+          <strong className="main_item_info_time">15시 00분</strong>
+          <strong>치아 검진</strong>
 
-        <div>
-          <strong>코카스페니얼</strong>
-          <span> 6세 | 남아</span>
+          <div>
+            <strong>코카스페니얼</strong>
+            <span> 6세 | 남아</span>
+          </div>
+        </div>
+
+        <div className="main_item_tag_wrapper">
+          <div className="">
+            <Tag icon={<ClockCircleOutlined />} color="default">
+              예약 대기 중
+            </Tag>
+            <Tag icon={<CheckCircleOutlined />} color="success">
+              예약 확정
+            </Tag>
+            <Tag icon={<CloseCircleOutlined />} color="error">
+              예약 거절
+            </Tag>
+          </div>
+
+          <div className="main_btn_wrapper">
+            <button className="btn_cancel btn_middle">예약 취소</button>
+            <button className="btn_middle">예약 확정</button>
+          </div>
         </div>
       </div>
-
-      <div className="main_item_tag_wrapper">
-        <div className="">
-          <Tag icon={<ClockCircleOutlined />} color="default">
-            예약 대기 중
-          </Tag>
-          <Tag icon={<CheckCircleOutlined />} color="success">
-            예약 확정
-          </Tag>
-          <Tag icon={<CloseCircleOutlined />} color="error">
-            예약 거절
-          </Tag>
-        </div>
-
-        <div className="main_btn_wrapper">
-          <button className="btn_cancel btn_middle">예약 취소</button>
-          <button className="btn_middle">예약 확정</button>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/WaitingListItem/index.tsx
+++ src/components/WaitingListItem/index.tsx
@@ -1,31 +1,34 @@
 import { FieldTimeOutlined, PhoneOutlined } from "@ant-design/icons";
 import { Tag } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/pages/main.scss";
 
 export default function WaitingListItem() {
   return (
-    <div className="waiting_list">
-      <div className="info_wrapper">
-        <h2 className="info_reserve_time">
-          <Tag color="lime">
-            <FieldTimeOutlined />
-            <span> 오후 3시 30분</span>
-          </Tag>
-        </h2>
+    <Link to={"/reservation/id"}>
+      <div className="waiting_list">
+        <div className="info_wrapper">
+          <h2 className="info_reserve_time">
+            <Tag color="lime">
+              <FieldTimeOutlined />
+              <span> 오후 3시 30분</span>
+            </Tag>
+          </h2>
 
-        <img className="profile" src="/images/logo/logo-3.png" />
-        <h2>차차</h2>
-        <div className="profile_species">코커스패니얼</div>
-        <div className="profile_age_sex">6살 | 남아</div>
-        <div className="info_category">
-          <Tag color="magenta">예방접종</Tag>
-        </div>
-        <div>
-          <PhoneOutlined />
-          <span className="info_specific">010-2345-6789</span>
+          <img className="profile" src="/images/logo/logo-3.png" />
+          <h2>차차</h2>
+          <div className="profile_species">코커스패니얼</div>
+          <div className="profile_age_sex">6살 | 남아</div>
+          <div className="info_category">
+            <Tag color="magenta">예방접종</Tag>
+          </div>
+          <div>
+            <PhoneOutlined />
+            <span className="info_specific">010-2345-6789</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
